4822be09efd202fd1d7b636d5e27a2d73f967d7c
jamshid <jamik@.com>
2022-03-06 10:51:39 +0500

    javascript

---
+++ script.js
@@ -0,0 +1,157 @@
+
+// var ism = 'Jamshid';
+// console.log(ism);
+
+// var age =22;
+// console.log(age);
+
+// console.log('6 + 4 = ' + 10);
+
+// console.log('25 - 10 = ' + (100-85));
+
+// console.log('49 / 7 = ' + 7);
+
+// console.log('4 * 8 = ' + 32);
+
+// var ism = prompt('Ismingizni kiriting');
+// var age = prompt('Yoshingizni kiriting');
+// var nun = prompt('25 + 5 = ');
+// var srt = prompt('6 * 4 = ');
+
+// alert('Assalomu alaykum ' + ism);
+// alert('Sizning Yilingiz ' + (2022-age));
+// alert(nun + (' javobiz tugri '));
+// alert(srt + ' javobiz tugri ');
+   
+
+// var ism = 'Jamshid';
+// var age = 22;
+// var bool = true;
+// var und;
+
+// console.log(ism);
+// console.log(age);
+// console.log(bool);
+// console.log(und);
+
+// console.log('Mening ismim ' + ism + '.Men ' + age + ' yoshdaman');
+
+// console.log(10 + 3);
+// console.log(10 - 3);
+// console.log(10 * 3);
+// console.log(10 / 3)
+// console.log(10 % 3);
+
+// console.log('6 + 3 = ' + 6 + 3); 
+
+// var str = prompt('Ismimgizni kiriting');
+// alert('Salom ' + str);
+// console.log(+str); //+ matn kurinishdan son kurinishga utkazib beradi!
+
+// var num = +prompt('Yoshingizni kiriting');
+// alert('Sizning yilingiz ' + (2022 - num)); 
+
+// console.log(typeof 'jamshid');
+// console.log(typeof 123);
+// console.log(typeof true);
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+// console.log('qwe');
+// console.warn('salom');
+// console.error('hello world!');
+
+
+
+//  var javob = +prompt('6 + 3 = ?');
+//  if(javob === 9){
+//     alert('javobingiz tugri!')
+//  }
+
+//  if(javob != 9){   
+//     alert('javobingiz notugri!')
+//  }
+
+//  ikkinchi yuli if ni urniga else iwlatsa ham buladi else{ alert{'javobingiz notugri'}}
+
+// if(true){
+//     alert('abdullajon')
+// }
+ 
+// let ism = 'Jamshid';
+// let text = 'salom ' + ism;
+// console.log(text);
+
+// 
+
+// null
+
+// let ozgaruvchi = 123;
+//  ozgaruvchi = null;
+// console.log(ozgaruvchi);
+
+// Undefined
+
+// let son
+// console.log(son);
+
+
+// Ozgaruvchiga nom berish
+
+// ozgaruvchiga js ning kalit suzlarini berib bumaydi
+
+// let $ism _ism = 'Jamshid' $ _ mumkin qolganlari mumkin emas
+
+
+// let ism = prompt ('ismingizni kiriting');
+// alert( 'Salom ' + ism);
+
+// let age = prompt ( 'Yoshingizni kiriting');
+// alert('siz ' + (2022-age + (' yilsiz')));
+
+
+// Operatorlar
+
+// matematik amallar
+
+//  x + y => (+) -> Operator , (x va y) -> Operantlar;
+//  x = 3 => (=) -> Operator , (x va 3) -> Operantlar;
+
+// unary => 1 ta operant bn bolgan operator;
+// +3, +"3", +"-3", -"3.14" +true, -false, +null, -"infinity" x++, ++x;
+
+// binary => 2 ta operant bilan bolgan operator;
+// let x = 5;
+// let x = 3 + 5;
+// let x = 3 + 5 * 2;
+
+// ternary => 3 ta operant bilan bolgan operator;
+//   let age = prompt('Yoshingizni kiriting')
+//   console.log( age > 18 ? 'pasport beriladi' : 'pasport berilmaydi');
+
+// let age = 30;
+// let isAdult = age > 18 ? "xa" : "yoq";
+// console.log(isAdult);
+
+// assigment (tayinlov) operators => ( =, + , - , * , / , +=, -=, /=, %, %=,);
+
+
+
+let ism = prompt('Ismimgizni kiriting');
+let age = +prompt('Yoshingizni kiriting');
+let misol_1 = +prompt('Misol 1) ' + '6 + 3 = ');
+let misol_2 = +prompt('Misol 2) ' + '20 - 5 = ');
+let misol_3 = +prompt('Misol 3) ' + '15 * 2 = ');
+let misol_4 = +prompt('Misol 4) ' + '8 / 4 = ');
+let misol_5 = +prompt('Misol 5) ' + '10 % 3 = ');
+
+console.log('Mening ismim ' + ism);
+console.log('Mening yoshim ' + age);
+console.log('6 + 3 = ' + 9 + ' Sizning javobingiz 9');
+console.log('20 - 5 = ' + 15 + ' Sizning javobingiz 15');
+console.log('15 * 2 = ' + 30 + ' Sizning javobingiz 30');
+console.log('8 / 4 = ' + 2 + ' Sizning javobingiz 2');
+console.log('10 % 3 = ' + 1 + ' Sizning javobingiz 1');
+
+
+
